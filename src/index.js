a669a349e72d33c5a7ac52bc4302739d11e35f4a
shonubijerry <shonubijerry@gmail.com>
2019-08-20 00:14:26 +0100

    [#167727303] Setup API documentation using swagger
    Install swagger-ui-express dependency
    Create a swagger.json document file in /public/api-docs
    serve swagger.json document file to swagger
    
    Bable was also setup to run the server using ES6 syntax

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import cors from 'cors';
+import swaggerUI from 'swagger-ui-express';
+import swaggerDocument from '../public/api-docs/swagger.json';
 import constants from './config/constants';
 
 const app = express();
-const port  = constants.PORT || 3000;
+const port = constants.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(logger('dev'));
 
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
