f59aef71b11ac352a852dafc9d3e6577499d9c50
stephen <stepheng323@gmail.com>
2019-08-20 21:07:09 +0100

    chore(dotenv):setup dotenv

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,20 +1,27 @@
+/* eslint-disable require-jsdoc */
 import dotenv from 'dotenv';
 
 dotenv.config();
-
 const {
-  DB_HOST, DB_USER, DB_NAME, DB_PASSWORD,
-  DB_IDLE_TIMEOUT_MILLISECONDS, DB_PORT,
-  DB_MAX, PORT
+	DEV_DATABASE_URL,
+	TEST_DATABASE_URL,
+	PROD_DATABASE_URL,
+	PORT
 } = process.env;
 
 module.exports = {
-  DB_HOST,
-  DB_USER,
-  DB_NAME,
-  DB_PASSWORD,
-  DB_PORT,
-  DB_MAX,
-  DB_IDLE_TIMEOUT_MILLISECONDS,
-  PORT
+	port: PORT,
+	development: {
+		url: DEV_DATABASE_URL,
+		dialect: 'postgres'
+	},
+	test: {
+		url: TEST_DATABASE_URL,
+		dialect: 'postgres'
+	},
+	production: {
+		use_env_variable: 'DATABASE_URL',
+		url: PROD_DATABASE_URL,
+		dialect: 'postgres'
+	}
 };
